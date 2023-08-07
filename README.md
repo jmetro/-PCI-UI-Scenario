bcee21d1760cdbf8e6799fb23464ef5b10e6d6ef
Joms <jomsmetro@gmail.com>
2023-08-07 21:12:54 +0800

    Added title props to Grid.tsx Added side effect to update document title Added title header in Grid.tsx

--- README.md
+++ README.md
@@ -64,3 +64,9 @@ Within this repo is a demo UI that our team needs to make some changes to before
 7. Lastly, this grid is going to go to production soon, and other developers will begin doing work on this project. While creating this demo, considerations were not made to keep the code clean and organized. Please organize this codebase so that it is easier to find and make changes to components, as well as finding any interfaces, types, or helper functions.
 
 After completing this project, please push any changes you made to a publicly accessible GitLab or GitHub repository and include that link in an email to your contact at PCI. Thank you!
+
+
+Task 1 Assumptions
+- Assumption - I'm expecting it to be scalable and there will be other type of tables or pages that will have different titles. 
+- this is the reason I put a side-effect to change the title instead of changing it directly in index.html
+- Added title pros as well
--- src/Grid.tsx
+++ src/Grid.tsx
@@ -3,6 +3,7 @@ import { ColDef } from "ag-grid-community";
 import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+import { useEffect } from "react";
 
 const columnDefs: ColDef[] = [
   { field: "designation", headerName: "Designation" },
@@ -17,9 +18,17 @@ const columnDefs: ColDef[] = [
   { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
 ];
 
-const NeoGrid = (): JSX.Element => {
+const NeoGrid = ({ title }: {title: string}): JSX.Element => {
+  useEffect(()=> {
+    // Assumption - I'm expecting it to be scalable and there will be other type of tables or pages that will have different titles. 
+    // this is the reason I put a side-effect to change the title instead of changing it directly in index.html
+    // Added title pros as well
+    document.title = title;
+  }, [title]);
+
   return (
     <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
+      <h1>{title}</h1>
       <AgGridReact
         rowData={data}
         columnDefs={columnDefs}
